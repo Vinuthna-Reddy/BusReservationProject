e66967b1fe2876a8283cf2aa5b83b981e84c90b0
Reventh Rao <reventhrao.anugu@lntinfotech.com>
2021-01-07 16:13:44 +0530

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeatsComponent } from './seats/seats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeatsComponent
   ],
   imports: [
     BrowserModule,
